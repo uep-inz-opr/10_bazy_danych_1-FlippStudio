b4e619b1d13183116ff08a92b2c043cba4e9cfc0
Flipp <glowski.f@gmail.com>
2022-05-03 16:08:20 +0200

    finished task

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import csv
+import sqlite3
+
+con = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER,
+                  to_subscriber data_type INTEGER,
+                  datetime data_type timestamp,
+                  duration data_type INTEGER ,
+                  celltower data_type INTEGER);''')
+
+
+def read_data(filename):
+    with open(filename, 'r') as fin:
+        reader = csv.reader(fin, delimiter=";")
+        next(reader, None)  # skip the headers
+
+        rows = [x for x in reader]
+        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) "
+                        "VALUES ( ?, ?, ?, ?, ?);", rows)
+        con.commit()
+
 
 if __name__ == "__main__":
-    pass
+    read_data(input())
+    cur.execute('SELECT sum(duration) FROM polaczenia')
+    print(cur.fetchone()[0])
